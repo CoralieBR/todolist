0c9c24ef3ad1e927c197786f44c8228a3b6d2980
Coralie <coralie.burtin@protonmail.com>
2024-07-26 14:44:50 +0200

    fix login

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,26 +2,16 @@
 
 namespace App\Tests\Controller;
 
-use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends WebTestCase
 {
-    private KernelBrowser|null $client = null;
-
-    public function setUp(): void
-    {
-        $this->client = static::createClient();
-    }
-
     public function testHomepageIsUp()
     {
-        $urlGenerator = $this->client->getContainer()->get('router.default');
+        $client = static::createClient();
+        $urlGenerator = $client->getContainer()->get('router.default');
 
-        $this->client->request(Request::METHOD_GET, $urlGenerator->generate('homepage'));
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $client->request('GET', '/');
+        $this->assertResponseIsSuccessful();
     }
 }
